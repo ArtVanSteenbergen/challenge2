2c2727b1beef1d9d75f0d96533f8b20558ffd2aa
ArtVanSteenbergen <47633479+ArtVanSteenbergen@users.noreply.github.com>
2019-03-13 16:25:53 +0100

    code cleanup

--- js/script.js
+++ js/script.js
@@ -1,92 +1,79 @@
 $(document).ready(function() {
-  var tl = new TimelineMax(),
+  // all the constants
+  const
+  HTML = $('html'),
+  POINTERH = $('.clockH'),
+  POINTERM = $('.clockI'),
+  POINTERS = $('.clockS'),
+  DIGITALCLOCK = $('#digitalClock'),
+  ANALOGCLOCK = $('#analogClock'),
+  DATEONSCREEN = $('#date'),
+  RIPLEY = $('#ripley'),
+  RIPLEYSPEAKS = $('#ripleySpeaks'),
+  UTCTOGGLE = $('#UtcToggle'),
+  UNMUTEBUTTON = $('#unmute'),
+  MUTEBUTTON = $('#mute'),
+
+  DAYSOFWEEK = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+  MONTHSOFYEAR = ['January','February','March','April','May','June','July','August','September','October','November','December'],
+  
+  ONESECOND = 1,
+  ONEHOUR = 60 * 60, 
+  TWELVEHOURS = 12 * 60 * 60,
+  FULLDAY = 24 * 60 * 60;
+
+
+  // all the variables
+  var
+  tl = new TimelineMax(),
   timeOffset = 0,
   timeOffsetHours = 0,
   timeOffsetMinutes = 0,
   timeOffsetSeconds = 0,
 
-  daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
-  monthsOfYear = ['January','February','March','April','May','June','July','August','September','October','November','December'],
   datetime = new Date(Date.now() + timeOffset),
   h = datetime.getHours(),
   i = datetime.getMinutes(),
   s = datetime.getSeconds(),
-  n = daysOfWeek[datetime.getDay()],
+  n = DAYSOFWEEK[datetime.getDay()],
   d = datetime.getDate(),
-  m = monthsOfYear[datetime.getMonth()],
+  m = MONTHSOFYEAR[datetime.getMonth()],
   Y = datetime.getFullYear(),
   date = '',
-
-  html = $('html'),
-  pointerH = $('.clockH'),
-  pointerM = $('.clockM'),
-  pointerS = $('.clockS'),
-  digitalClock = $('#digitalClock'),
-  analogClock = $('#analogClock'),
-  dateOnScreen = $('#date'),
-  ripley = $('#ripley'),
-  ripleySpeaks = $('#ripleySpeaks'),
-  utcToggle = $('#utcToggle'),
-  unmuteButton = $('#unmute'),
-  muteButton = $('#mute'),
 
   ripleyCurrentHour = 0,
   his = '00:00:00',
   utc = false,
 
-  oneSecond = 60 / 60, // 1 second
-  oneHour = 60 * 60, //1 hour tween
-  twelveHours = 12 * 60 * 60, //12 hour tween
-  fullDay = 24 * 60 * 60, //24 hour tween
-  
   mute = true,
+
   bell = new Audio('bell.mp3'),
-  tick = new Audio('tick.mp3');
+  tick = new Audio('tick.mp3'),
 
-  TweenMax.set('.second, .hour, .minute', {
-    yPercent: -50,
-    transformOrigin: '50% 100%'
-  });
+  init = new TimelineMax(),
 
-  var timeTween = TweenMax.to(html, fullDay, {
-    repeat: -1
-  });
-  var init = new TimelineMax();
+  // set rotation for every pointer and set a 24h timetween
+  hourTween = TweenMax.to(POINTERH, TWELVEHOURS, {rotation: '360', ease: Linear.easeNone, repeat: -1, paused: true}),
+  minuteTween = TweenMax.to(POINTERM, ONEHOUR, {rotation: '360', ease: Linear.easeNone, repeat: -1, paused: true}),
+  secondsTween = TweenMax.to(POINTERS, ONESECOND, {rotation: '360', ease: Linear.easeNone, repeat: -1, paused: true}),
+  timeTween = TweenMax.to(HTML, FULLDAY, {repeat: -1});
 
-  init.set(dateOnScreen, {y: '50%', x:'-50%', autoAlpha: 0})
-  .fromTo('html', 2,{backgroundImage: 'url()',backgroundPosition: '0 500%', backgroundRepeat: 'no-repeat'},{backgroundImage: 'url(img/mars.png)',backgroundPosition: '0 100%', backgroundRepeat: 'no-repeat'})
+  // initial animations to set up elements in a "beautiful" way
+  init.set('.second, .hour, .minute', {yPercent: -50, transformOrigin: '50% 100%'}).set(DATEONSCREEN, {y: '50%', x:'-50%', autoAlpha: 0})
+  .fromTo(HTML, 2,{backgroundImage: 'url()',backgroundPosition: '0 500%', backgroundRepeat: 'no-repeat'},{backgroundImage: 'url(img/mars.png)',backgroundPosition: '0 100%', backgroundRepeat: 'no-repeat'})
   .from('footer', 2,{y: '-1000%', autoAlpha: 0,  ease: Bounce.easeOut}, '-=2')
-  .from(analogClock, 2,{rotationX: '90deg',y: '-200px', autoAlpha: 0,  ease: Elastic.easeOut}, '-=1')
+  .from(ANALOGCLOCK, 2,{rotationX: '90deg',y: '-200px', autoAlpha: 0,  ease: Elastic.easeOut}, '-=1')
   .staggerFrom('footer div', 0.5, {x: '20px', autoAlpha: 0}, 0.1, '-=0.5')
-  .fromTo(digitalClock, 3, {y: '50%', x:'-50%', autoAlpha: 0},{y: '-50%',x: '-50%', autoAlpha: 1, ease: Elastic.easeOut}, '-=3')
-  .to(dateOnScreen, 3, {y: '-50%',x: '-50%', autoAlpha: 1, ease: Elastic.easeOut}, '-=3')
+  .fromTo(DIGITALCLOCK, 3, {y: '50%', x:'-50%', autoAlpha: 0},{y: '-50%',x: '-50%', autoAlpha: 1, ease: Elastic.easeOut}, '-=3')
+  .to(DATEONSCREEN, 3, {y: '-50%',x: '-50%', autoAlpha: 1, ease: Elastic.easeOut}, '-=3')
   .to('main', 3, {backgroundColor: 'rgba(0,0,0,0.5)', ease: Elastic.easeOut}, '-=3')
   .to('main', 1.5, {backgroundColor: 'rgba(0,0,0,0.2)', ease: Linear.easeNone}, '-=1.5');
 
 
-  var hourTween = TweenMax.to(pointerH, twelveHours, {
-    rotation: '360',
-    ease: Linear.easeNone,
-    repeat: -1,
-    paused: true
-  });
-
-  var minuteTween = TweenMax.to(pointerM, oneHour, {
-    rotation: '360',
-    ease: Linear.easeNone,
-    repeat: -1,
-    paused: true
-  });
-
-  var secondsTween = TweenMax.to(pointerS, oneSecond, {
-    rotation: '360',
-    ease: Linear.easeNone,
-    repeat: -1,
-    paused: true
-  });
-
+  // return st, nd, rd or th to day of month
   function nth(d) {
-    if (d > 3 && d < 21) return d+'th'; 
+    if (d > 3 && d < 21)
+      return d+'th'; 
     switch (d % 10) {
       case 1:  return d+"st";
       case 2:  return d+"nd";
@@ -98,8 +85,8 @@ $(document).ready(function() {
   // let Ripley tell the time hourly with a 'Ding' if audio isn't muted
   function hoursShow(h) {
     ripleyCurrentHour=0;
-    tl.fromTo(ripley, 1, {top: '600px', ease: Expo.easeOut},{top: '0px', ease: Expo.easeOut, onComplete:speak}, 'ripley')
-    .fromTo(ripleySpeaks,0.25,{y: '20px', x: '40px', autoAlpha: 0, ease: Expo.easeOut},{y: '0px',x: '0px',autoAlpha: 1,ease: Expo.easeOut}, 'ripley +0.1')
+    tl.fromTo(RIPLEY, 1, {top: '600px', ease: Expo.easeOut},{top: '0px', ease: Expo.easeOut, onComplete:speak}, 'ripley')
+    .fromTo(RIPLEYSPEAKS,0.25,{y: '20px', x: '40px', autoAlpha: 0, ease: Expo.easeOut},{y: '0px',x: '0px',autoAlpha: 1,ease: Expo.easeOut}, 'ripley +0.1')
     .yoyo(true).paused(true);
 
     tl.restart().repeat(h*2-1);
@@ -114,7 +101,6 @@ $(document).ready(function() {
     }
     $('#ripleySpeaks').html(ripleyCurrentHour);
   }
-
 
   // add time or retrects time per pointer in milliseconds
   function setTimeOffset(type, amount) {
@@ -133,10 +119,8 @@ $(document).ready(function() {
   }
 
   function showDate(date) {
-    if (dateOnScreen.html() != '') {
-      TweenMax.fromTo(dateOnScreen, 3, {y: '-150%',x: '-50%', autoAlpha: 1, ease: Elastic.easeOut},{y: '-50%',x: '-50%', autoAlpha: 1, ease: Elastic.easeOut});
-    }
-    dateOnScreen.html(date);
+    if (DATEONSCREEN.html() != '') {TweenMax.fromTo(DATEONSCREEN, 3, {y: '-150%',x: '-50%', autoAlpha: 1, ease: Elastic.easeOut},{y: '-50%',x: '-50%', autoAlpha: 1, ease: Elastic.easeOut});}
+    DATEONSCREEN.html(date);
   }
 
   // display the clock in 6 digits in the #clock element
@@ -145,7 +129,7 @@ $(document).ready(function() {
     i = (i < 10) ? '0' + i : i;
     s = (s < 10) ? '0' + s : s;
     his = h + ':' + i + ':' + s;
-    TweenMax.set(digitalClock,{text:{value: his}});
+    TweenMax.set(DIGITALCLOCK,{text:{value: his}});
     TweenMax.set('title',{text:{value: his}});
   }
 
@@ -160,26 +144,19 @@ $(document).ready(function() {
     h = datetime.getHours();
     i = datetime.getMinutes();
     s = datetime.getSeconds();
-    n = daysOfWeek[datetime.getDay()];
+    n = DAYSOFWEEK[datetime.getDay()];
     d = datetime.getDate();
-    m = monthsOfYear[datetime.getMonth()];
+    m = MONTHSOFYEAR[datetime.getMonth()];
     Y = datetime.getFullYear();
 
     date = n + ', ' + m + ' ' +  nth(d) + ' ' + Y;
-    if (dateOnScreen.html() != date) {
-      showDate(date);
-    }
+    if (DATEONSCREEN.html() != date) {showDate(date);}
 
     // convert 24 to 12 and show the time hourly
     if (i < 1 && s < 1) {
-      if (h == 0) {
-        hoursShow(12);
-      }
-      else if (h <= 12) {
-        hoursShow(h);
-      } else {
-        hoursShow(h-12);
-      }
+      if (h == 0) {hoursShow(12);}
+      else if (h <= 12) {hoursShow(h);}
+      else {hoursShow(h-12);}
     }
 
     // play sound on the half hour mark when audio isn't muted
@@ -199,24 +176,16 @@ $(document).ready(function() {
     secondsAsSeconds = s / 60;
 
     // progress from 0 to 1 per pointerTween
-    hourTween.progress(hoursAsSeconds / twelveHours);
-    timeTween.progress(hoursAsSeconds/ fullDay);
-    minuteTween.progress(minutesAsSeconds / oneHour);
-    secondsTween.progress(secondsAsSeconds / oneSecond);
+    hourTween.progress(hoursAsSeconds / TWELVEHOURS);
+    timeTween.progress(hoursAsSeconds/ FULLDAY);
+    minuteTween.progress(minutesAsSeconds / ONEHOUR);
+    secondsTween.progress(secondsAsSeconds / ONESECOND);
 
     // change background based on time of day
-    if (timeTween.progress() >= 0 && timeTween.progress() < 0.25) {
-      TweenMax.to('html', 2, {backgroundColor: '#000000'});
-    }
-    if (timeTween.progress() >= 0.25 && timeTween.progress() < 0.5) {
-      TweenMax.to('html', 2, {backgroundColor: '#e09572'});
-    }
-    if (timeTween.progress() >= 0.5 && timeTween.progress() < 0.75) {
-      TweenMax.to('html', 2, {backgroundColor: '#e0bfb0'});
-    }
-    if (timeTween.progress() >= 0.75 && timeTween.progress() < 1) {
-      TweenMax.to('html', 2, {backgroundColor: '#94624b'});
-    }
+    if (timeTween.progress() >= 0 && timeTween.progress() < 0.25) {TweenMax.to(HTML, 2, {backgroundColor: '#000000'});}
+    if (timeTween.progress() >= 0.25 && timeTween.progress() < 0.5) {TweenMax.to(HTML, 2, {backgroundColor: '#e09572'});}
+    if (timeTween.progress() >= 0.5 && timeTween.progress() < 0.75) {TweenMax.to(HTML, 2, {backgroundColor: '#e0bfb0'});}
+    if (timeTween.progress() >= 0.75 && timeTween.progress() < 1) {TweenMax.to(HTML, 2, {backgroundColor: '#94624b'});}
 
   }
 
@@ -226,22 +195,42 @@ $(document).ready(function() {
   // set time every second
   setInterval(function() {
     showTime();
-      if (!mute) {
-        tick.volume = 0.01;
-        tick.play();
-      }
-
+    // play tick every second if sound isn't muted
+    if (!mute) {
+      tick.volume = 0.01;
+      tick.play();
+    }
   }, 1000);
 
-  // onclick events for footer controls
+  // onclick events and footer controls
   $('#addHour').click(() => setTimeOffset('hours', 1));
   $('#subtractHour').click(() => setTimeOffset('hours', -1));
   $('#addMinute').click(() => setTimeOffset('minutes', 1));
   $('#subtractMinute').click(() => setTimeOffset('minutes', -1));
   $('#addTenSeconds').click(() => setTimeOffset('seconds', 10));
   $('#subtractTenSeconds').click(() => setTimeOffset('seconds', -10));
-  $('#resetTimeOffset').click(() => {timeOffsetHours = 0;timeOffsetMinutes = 0;timeOffsetSeconds = 0;utc = true});
-  muteButton.click(()=>{mute = true; TweenMax.to(muteButton, 0.5, {x: '-23px',autoAlpha:0});  TweenMax.to(unmuteButton, 0.5, {x: '0px',autoAlpha:1}); });
-  unmuteButton.click(()=>{mute = false; TweenMax.to(unmuteButton, 0.5, {x: '0px',autoAlpha:0});  TweenMax.to(muteButton, 0.5, {x: '-23px', autoAlpha:1}); });
-  utcToggle.click(()=>{if (!utc) {utc = true; TweenMax.to(utcToggle, 1, {color: 'green'}); } else {utc = false; TweenMax.to(utcToggle, 1, {color: 'red'});}});
+  $('#resetTimeOffset').click(() => {
+    timeOffsetHours = 0;
+    timeOffsetMinutes = 0;
+    timeOffsetSeconds = 0;
+    utc = true;
+  });
+  MUTEBUTTON.click(()=>{
+    mute = true;
+    TweenMax.to(MUTEBUTTON, 0.5, {x: '-23px',autoAlpha:0});
+    TweenMax.to(UNMUTEBUTTON, 0.5, {x: '0px',autoAlpha:1});
+  });
+  UNMUTEBUTTON.click(()=>{
+    mute = false;
+    TweenMax.to(UNMUTEBUTTON, 0.5, {x: '0px',autoAlpha:0});
+    TweenMax.to(MUTEBUTTON, 0.5, {x: '-23px', autoAlpha:1});
+  });
+  UTCTOGGLE.click(()=>{
+    if (!utc) {
+      utc = true;
+      TweenMax.to(UTCTOGGLE, 1, {color: 'green'});
+    } else {
+      utc = false;
+      TweenMax.to(UTCTOGGLE, 1, {color: 'red'});}
+    });
 });
